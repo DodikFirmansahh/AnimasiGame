3bb0cc9489c6ed39323bd3748b6d9132d03f74e3
DodikFirmansahh <134833762+DodikFirmansahh@users.noreply.github.com>
2023-07-05 14:44:20 +0700

    Runner_3d_Matematich

---
+++ Script/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isEscapeToExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void MenuAwal()
+    {
+        SceneManager.LoadScene("MenuAwal");
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+
+}
